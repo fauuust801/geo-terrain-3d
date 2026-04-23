a33b4add2caeb8a38a28745bedde4f735f0782a2
fauuust801 <fauuust801@gmail.com>
2026-04-24 01:27:00 +0900

    feat: 创建 GitHub Actions 自动部署文件

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import glsl from 'vite-plugin-glsl'
 import path from 'path'
 
 export default defineConfig({
-  // ✅ 新增这一行，把 your-repo-name 换成你的 GitHub 仓库名
-  base: '/geo-terrain-3d/',
+base: '/geo-terrain-3d/',
   
   plugins: [
     react(),
